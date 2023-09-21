58e7e69c3bbb66fc8b39e14bca342ac452cf853f
xlyxs <ilyes.djebara@etu.usthb.dz>
2023-09-21 15:31:20 +0100

    boilerplate + responsive navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import Header from './components/Header';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#252B48',
+      light: '#445069',
+      dark: '#0d1117',
+      contrastText: '#F1EFEF',
+    },
+    secondary: {
+      main: '#5B9A8B',
+      light: '#F7E987',
+    }
+  },
+});
+
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
+    </ThemeProvider >
   );
 }
 
